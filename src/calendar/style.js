6a8c122b64da2e88985f5f53e18a6636304aa822
nirobon <nirobon_saeku@hotmail.com>
2018-08-02 22:06:16 +0700

    edit

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -1,29 +1,29 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import * as defaultStyle from '../style';
 
 const STYLESHEET_ID = 'stylesheet.calendar.main';
 
-export default function getStyle(theme={}) {
-  const appStyle = {...defaultStyle, ...theme};
-  return StyleSheet.create({
-    container: {
-      paddingLeft: 5,
-      paddingRight: 5,
-      backgroundColor: appStyle.calendarBackground
-    },
-    monthView: {
-      backgroundColor: appStyle.calendarBackground
-    },
-    week: {
-      marginTop: 7,
-      marginBottom: 7,
-      flexDirection: 'row',
-      justifyContent: 'space-around'
-    },
-    dayContainer: {
-      width: 32
-    },
-    ...(theme[STYLESHEET_ID] || {})
-  });
+export default function getStyle(theme = {}) {
+	const appStyle = { ...defaultStyle, ...theme };
+	return StyleSheet.create({
+		container: {
+			paddingLeft: appStyle.pdLeft ? appStyle.pdLeft : 5,
+			paddingRight: appStyle.pdRight ? appStyle.pdRight : 5,
+      backgroundColor: appStyle.calendarBackground,
+      flex: 1,
+		},
+		monthView: {
+			backgroundColor: appStyle.calendarBackground,
+		},
+		week: {
+			marginTop: appStyle.appMarginTop ? appStyle.appMarginTop : 7,
+			marginBottom: appStyle.appMarginBottom ? appStyle.appMarginBottom : 7,
+			flexDirection: 'row',
+			justifyContent: 'space-around',
+		},
+		dayContainer: {
+			width: appStyle.dayContain ? appStyle.dayContain : 32,
+		},
+		...(theme[STYLESHEET_ID] || {}),
+	});
 }
-
