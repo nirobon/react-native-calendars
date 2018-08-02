eee61699015543d5fc487f89d516860a23310246
nirobon <nirobon_saeku@hotmail.com>
2018-08-02 22:04:38 +0700

    edit

--- src/calendar/day/basic/style.js
+++ src/calendar/day/basic/style.js
@@ -7,46 +7,51 @@ export default function styleConstructor(theme={}) {
   const appStyle = {...defaultStyle, ...theme};
   return StyleSheet.create({
     base: {
-      width: 32,
-      height: 32,
-      alignItems: 'center'
+      width:appStyle.wbase ? appStyle.wbase : 32,
+      height: appStyle.hbase ? appStyle.hbase : 32,
+      alignItems: 'center',
+      padding: 2,  
     },
     text: {
-      marginTop: Platform.OS === 'android' ? 4 : 6,
+      marginTop:appStyle.mgtop ? appStyle.mgtop : 1,
+      marginBottom: 1,
       fontSize: appStyle.textDayFontSize,
       fontFamily: appStyle.textDayFontFamily,
-      fontWeight: '300',
+      fontWeight:appStyle.ttfontWeight  ?appStyle.ttfontWeight: '300',
       color: appStyle.dayTextColor,
-      backgroundColor: 'rgba(255, 255, 255, 0)'
+      backgroundColor: 'rgba(255, 255, 255, 0)',
     },
     alignedText: {
-      marginTop: Platform.OS === 'android' ? 4 : 6
+      marginTop: Platform.OS === 'android' ? 4 : 4,
+      marginBottom: Platform.OS === 'android' ? 4 : 4,
     },
     selected: {
-      backgroundColor: appStyle.selectedDayBackgroundColor,
-      borderRadius: 16
-    },
-    today: {
-      backgroundColor: appStyle.todayBackgroundColor
+      borderColor: appStyle.selectedDayBackgroundColor,
+      borderRadius:appStyle.wBDradious ? appStyle.wBDradious : 16,
+      borderWidth: 1,
     },
     todayText: {
       color: appStyle.todayTextColor
     },
     selectedText: {
-      color: appStyle.selectedDayTextColor
+      color: appStyle.selectedDayTextColor,
+      fontWeight: '900',
     },
     disabledText: {
-      color: appStyle.textDisabledColor
+      color: appStyle.textDisabledColor,
+      textDecorationLine:'underline line-through',
     },
     dot: {
-      width: 4,
-      height: 4,
-      marginTop: 1,
-      borderRadius: 2,
-      opacity: 0
+      width:appStyle.showdot ?  4 : null,
+      height: appStyle.showdot ?  4 : null,
+      marginTop: appStyle.showdot ?  1 : null,
+      borderRadius: appStyle.showdot ?  2 : null,
+      opacity: appStyle.showdot ?  0 : null,
+      marginBottom: 4,
+      
     },
     visibleDot: {
-      opacity: 1,
+      // opacity: 1,
       backgroundColor: appStyle.dotColor
     },
     selectedDot: {
